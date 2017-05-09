bc825ea2b32a45894af7d7b2fcae01d8e0e991b1
Carl Wood <cjwood17@googlemail.com>
2017-05-09 22:19:37 +0100

    Connect to moviedb api and fetch film covers

---
+++ src/App.vue
@@ -0,0 +1,99 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 80px;
+}
+
+h1, h2, h3, h4, p {
+  margin-top: 0;
+}
+
+.container {
+  margin: 0 auto;
+  max-width: 1200px;
+  padding: 0 20px;
+}
+
+.logo {
+  color: #e0822c;
+  font-size: 28px;
+  letter-spacing: -2px;
+  position: absolute;
+  left: 20px;
+  top: 15px;
+}
+
+img {
+  max-width: 100%;
+}
+
+.btn {
+  border: 0;
+  cursor: pointer;
+  font-size: 16px;
+  padding: 10px;
+  text-decoration: none;
+}
+
+.btn--primary {
+  background: #e0822c;
+  color: #fff;
+}
+
+.btn--check {
+  background: #333;
+  color: #fff;
+  font-size: 11px;
+  letter-spacing: 3px;
+  text-transform: uppercase;
+}
+
+.btn--lg {
+  font-size: 20px;
+  padding: 10px 20px;
+}
+
+.btn--block {
+  display: block;
+  width: 100%;
+}
+
+input[type=text] {
+  display: block;
+  font-size: 16px;
+  padding: 10px;
+  width: 100%;
+}
+
+.page-title {
+  font-size: 28px;
+  letter-spacing: -1px;
+  text-align: left;
+}
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="container">
+    <h1 class="logo">{{ title }}</h1>
+    <h2 class="page-title">{{ prompt }}</h2>
+    <a href="#" class="btn btn--lg btn--primary" v-on:click="fetchFilms()">Generate quiz</a>
+    <p v-if="error">{{ error }}</p>
+    <ul class="covers list--unstyled" v-if="films">
+      <li v-for="(film, index) in films">
+        <div class="cover-img">
+          <img v-bind:src="getCover(index)">
+        </div>
+        <!-- <p>{{ film.title }}</p> -->
+        <input type="text" placeholder="Type film here...">
+        <button type="submit" class="btn btn--check btn--block">Check</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      title: 'fuzzfilms',
+      prompt: 'Can you name these popular films from their fuzzy film posters?',
+      films: '',
+      error: '',
+      imgBaseUrl: 'http://image.tmdb.org/t/p/w500/'
+    }
+  },
+  methods: {
+    fetchFilms () {
+      this.$http.get('https://api.themoviedb.org/3/movie/popular?api_key=9b1a22b01884b889ed192ba89db7edeb&language=en-US&page=1')
+        .then(response => {
+          this.films = response.data.results
+        }, response => {
+          this.error = response.statusText
+        })
+    },
+    getCover (index) {
+      return this.imgBaseUrl + this.films[index].poster_path
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+.covers {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-row-gap: 30px;
+  grid-column-gap: 30px;
+  margin-top: 60px;
+}
+
+@media (min-width: 680px) {
+  .covers {
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+
+@media (min-width: 920px) {
+  .covers {
+    grid-template-columns: repeat(4, 1fr);
+  }
+}
+
+@media (min-width: 1120px) {
+  .covers {
+    grid-template-columns: repeat(5, 1fr);
+  }
+}
+
+.cover-img {
+  margin-bottom: 5px;
+  position: relative;
+}
+
+.cover-img img {
+  filter: blur(7px);
+}
+
+.list--unstyled {
+  list-style: none;
+  padding: 0;
+}
+</style>
